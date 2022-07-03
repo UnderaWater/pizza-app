4a46e6b26491d33248c1e73a19b2f57d80c33cdc
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-07-03 13:20:36 +0300

    add: added components and basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import Categories from './components/Categories';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
 import './scss/app.scss';
 
 function App() {
   return (
     <div className="wrapper">
-      Test
-      <h2>Title</h2>
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">All pizzas</h2>
+          <div class="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meat</li>
+        <li>Vegetarian</li>
+        <li>Grill</li>
+        <li>Spicy</li>
+        <li>Closed</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
