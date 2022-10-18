c3729903d3693897be228b7057597d81daebf27a
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-10-18 23:10:22 +0200

    add: added skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import { Skeleton } from './components/PizzaBlock/Skeleton';
 import Sort from './components/Sort';
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className="wrapper">
       <Header />
@@ -16,11 +21,9 @@ function App() {
           </div>
           <h2 className="content__title">All pizzas</h2>
           <div className="content__items">
-            <PizzaBlock />
-            <PizzaBlock />
-            {pizzas.map(obj => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
